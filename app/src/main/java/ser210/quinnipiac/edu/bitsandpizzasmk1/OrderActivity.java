d2313a6578eb2f9712d3ae78e30d7bcd0331950d
vnigam <vishaldnigam@gmail.com>
2019-05-08 14:23:54 -0400

    bits+pizzas

---
+++ app/src/main/java/ser210/quinnipiac/edu/bitsandpizzasmk1/OrderActivity.java
@@ -0,0 +1,16 @@
+package ser210.quinnipiac.edu.bitsandpizzasmk1;
+
+import android.app.ActionBar;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class OrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+        ActionBar actionBar = getActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+}
